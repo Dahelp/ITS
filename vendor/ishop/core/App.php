c22834ea82f627f4dd95818a0e213cc9f1111ad2
Dahelp <85491467+Dahelp@users.noreply.github.com>
2025-04-03 10:08:16 +0300

    Тест автоматического деплоя

--- vendor/ishop/core/App.php
+++ vendor/ishop/core/App.php
@@ -10,7 +10,7 @@ class App{
     public static $app;
 
     public function __construct(){
-        $query = trim($_SERVER['QUERY_STRING'] ?? '', '/');
+        $query = trim($_SERVER['QUERY_STRING'] ?? '', '/');		
         session_start();
         self::$app = Registry::instance();
         $this->getParams();
